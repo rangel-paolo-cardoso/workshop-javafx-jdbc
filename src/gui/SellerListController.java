5241e4c74d1ec406e4826c0714841c36bf9121c7
Rangel Paolo <paolorangel1996@outlook.com>
2022-07-27 23:51:31 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -46,6 +47,16 @@ public class SellerListController implements Initializable, DataChangeListener {
     @FXML
     private TableColumn<Seller, String> tableColumnName;
 
+    // 
+    @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
+
     @FXML
     private TableColumn<Seller, Seller> tableColumnEdit;
 
@@ -76,6 +87,11 @@ public class SellerListController implements Initializable, DataChangeListener {
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
@@ -134,9 +150,10 @@ public class SellerListController implements Initializable, DataChangeListener {
                     return;
                 }
 
-                // setGraphic(button);
+                setGraphic(button);
                 // button.setOnAction(
-                //         event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+                //     // event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event))
+                //     );
             }
         });
     }
